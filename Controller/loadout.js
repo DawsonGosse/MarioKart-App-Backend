1be6eba99d261240fbc66ada24e3ffbf8abe747e
Kevin Adlparvar <kadlparvar@my.bcit.ca>
2022-03-21 13:42:37 -0700

    added create loadout controller

--- Controller/loadout.js
+++ Controller/loadout.js
@@ -7,6 +7,25 @@ const getLoadout = (req, res) => {
     })
 }
 
+// creates loadout
+
+const createLoadout = (req, res) => {
+    // copies(clones) loadout schema
+    const loadout = new Loadout()
+
+    loadout.character = req.body.character
+    loadout.kart = req.body.kart
+    loadout.wheels = req.body.wheels
+    loadout.glider = req.body.glider
+    
+    // saves the loudout
+    loadout.save((err, data) => {
+        if(err) return res.status(500).send("something went wrong")
+        res.status(201).json(data)
+    })
+    res.status(201).send('Loadout Created')
+}
+
 module.exports = {
     getLoadout,
     createLoadout,
